66eaf5fe31ffa668328acb34b36f04f8d731769e
Pablo Tilli <tillipablo@gmail.com>
2021-06-22 11:02:20 -0300

    Agregamos ruta estatica para imagenes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const usuariosRoutes = require('./routes/usuarios_routes');
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.use(express.json());
 
 app.use('/usuarios', usuariosRoutes);
